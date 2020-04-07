5fc4eba38e14d802172ec4dceb10c7401609f253
Malachi Payne <malachimpayne@gmail.com>
2020-04-07 12:22:00 -0700

    First commit with Java projects 🦁

---
+++ nestedLoopPractice.java
@@ -0,0 +1,19 @@
+public class nestedLoopPractice {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            for (int space = 1; space <= 10 - i; ++space) {
+                System.out.print("  ");
+            }
+            for (int j = i; j <= 2 * i - 1; ++j) {
+                System.out.print("◦ ");
+            }
+
+            for (int j = 0; j < i - 1; ++j) {
+                System.out.print("◦ ");
+            }
+
+            System.out.println();
+        }
+    }
+}
