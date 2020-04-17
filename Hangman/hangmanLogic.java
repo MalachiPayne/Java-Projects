2791f4932a77a2111faa68fc86162fe15820ec41
MalachiPayne <malachimpayne@gmail.com>
2020-04-16 22:36:21 -0700

    Added a few lines to the Hangman logic

--- Hangman/hangmanLogic.java
+++ Hangman/hangmanLogic.java
@@ -5,15 +5,44 @@ import java.util.Scanner;
 public class hangmanLogic {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        // Ask the user for a character
-        System.out.print("Enter an alphebetic character: ");
-        char character = scnr.next().charAt(0);
 
-        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+        String secretWord = "";
+        String guess = "";
+        int guessCount = 0;
+        int guessLimit = 3;
+        boolean outOfGuesses = false;
 
-            System.out.println("Thanks! [" + character + "] is valid.");
+        // ASK THE HOSTUSER FOR A WORD TO GUESS
+        // --------------------------------------------------------------------------------
+        System.out.println("Please enter the secret word: ");
+        secretWord = scnr.nextLine();
+
+        // PUT HOSTUSERS WORD INTO AN ARRAY
+        // --------------------------------------------------------------------------------
+        char[] characterArray = secretWord.toCharArray();
+        System.out.println("your secret word in an array: " + "" + new String(characterArray));
+
+        // HIDE THE HOSTUSERS WORD UNTIL GUESSED CORRECTLY
+        // --------------------------------------------------------------------------------
+        String hiddenSecretWord = secretWord;
+        hiddenSecretWord = secretWord.replaceAll(".", "_");
+        System.out.println(hiddenSecretWord);
+
+        while (!guess.equals(secretWord) && !outOfGuesses) {
+
+            if (guessCount < guessLimit) {
+                System.out.print("Enter a guess: ");
+                guess = scnr.nextLine();
+                guessCount++;
+            } else {
+                outOfGuesses = true;
+            }
+        }
+
+        if (outOfGuesses) {
+            System.out.println("You Lose! Correct word is: " + secretWord);
         } else {
-            System.out.println("Sorry [" + character + "] is not valid.");
+            System.out.println("You Win!");
         }
         scnr.close();
     }
