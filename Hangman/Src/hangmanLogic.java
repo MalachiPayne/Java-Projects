80761a33770c749b63d78cb39c42415b2d5a8202
MalachiPayne <malachimpayne@gmail.com>
2020-04-16 18:39:18 -0700

    Organized into folders

---
+++ Hangman/Src/hangmanLogic.java
@@ -0,0 +1,20 @@
+package Hangman.Src;
+
+import java.util.Scanner;
+
+public class hangmanLogic {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        // Ask the user for a character
+        System.out.print("Enter an alphebetic character: ");
+        char character = scnr.next().charAt(0);
+
+        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+
+            System.out.println("Thanks! [" + character + "] is valid.");
+        } else {
+            System.out.println("Sorry [" + character + "] is not valid.");
+        }
+        scnr.close();
+    }
+}
